40af02e2993f5fb904ee2e960f8e02e99281707b
LeeDongJun <dlthfwns3@naver.com>
2025-11-21 17:42:17 +0900

    data 추가 및 수정

--- src/com/team1/airline/dao/impl/DataManager.java
+++ src/com/team1/airline/dao/impl/DataManager.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
